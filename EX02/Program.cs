6c72a5ec0b72d47d484da51031aac6923cdf0b17
von_Krolikoff <milkbrother@yandex.ru>
2022-06-22 22:48:33 +0300

    +EX02

---
+++ EX02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine ("Считаем сумму цифр в предложенном числе.");
+Console.WriteLine ("Введите число - ");
+string input = Console.ReadLine();
+int sum = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    sum = sum + ((input [i]) - 48);
+    // Console.WriteLine (input [i] + "   " + sum);  !!! я не понимаю откуда берется 48 !!!
+}
+
+Console.WriteLine (sum);
