8ee2b276fc52250aa9b7c9ccb0e7db7090c7b149
von_Krolikoff <milkbrother@yandex.ru>
2022-06-25 10:12:13 +0300

    +EX03

--- EX02/Program.cs
+++ EX02/Program.cs
@@ -2,12 +2,30 @@
 Console.WriteLine ("Считаем сумму цифр в предложенном числе.");
 Console.WriteLine ("Введите число - ");
 string input = Console.ReadLine();
+int[] array = new int [input.Length];
 int sum = 0;
 
-for (int i = 0; i < input.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    sum = sum + ((input [i]) - 48);
-    // Console.WriteLine (input [i] + "   " + sum);  !!! я не понимаю откуда берется 48 !!!
+    array [i] = input [i] - 48; // Странно, но работает Оо. 
+}
+
+PrintArray (array);
+Console.WriteLine ();
+
+for (int i = 0; i < array.Length; i++)
+{
+    sum = sum + ((array [i]));
 }
 
 Console.WriteLine (sum);
+
+void PrintArray(int[] colPA)
+{
+    int count = colPA.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(colPA[pos] + " "); pos++;
+    }
+}
---
+++ EX03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Вывод сделать отдельным методом.
+int[] array = new int [8];
+FillArray (array);
+PrintArray (array);
+
+void FillArray(int[] colFA)
+{
+    int length = colFA.Length;
+    int index = 0;
+    while (index < length)
+    {
+        colFA[index] = new Random().Next(0, 99); index++;
+    }
+}
+
+void PrintArray(int[] colPA)
+{
+    int count = colPA.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(colPA[pos] + " "); pos++;
+    }
+}
